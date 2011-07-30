636a3d3575202c766152a4ae96acbb1e89e58592
Iurii Ogiienko <iurii.ogiienko@gmail.com>
2011-07-30 17:42:09 +0300

    collapse expand fixes

--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,4 @@
 
-var setQueueStates = function(){
-    $('#queues_list .queue_item').each(function(){
-        if($(this).hasClass('collapsed')){
-            collapseQueue(this);
-        }else{
-            expandQueue(this);
-        }
-    });
-
-    $('#queues_list .expand_queue').live('click', function(e){
-        e.preventDefault();
-        expandQueue(this);
-    });
-
-    $('#queues_list .collapse_queue').live('click', function(e){
-        e.preventDefault();
-        console.log('opa');
-        collapseQueue(this);
-    });
-};
-
-var collapseQueue = function(el){
-    var item = $(el);
-
-    if(! $(el).hasClass('queue_item')){
-        item = $(el).parents('.queue_item');
-    }
-    $(item).addClass('collapsed').find('.collapse_queue').hide().end().find('.expand_queue').show();
-
-};
-
-var expandQueue = function(el){
-    var item = $(el);
-    if(! $(el).hasClass('queue_item')){
-        item = $(el).parents('.queue_item');
-    }
-    $(item).removeClass('collapsed').find('.collapse_queue').show().end().find('.expand_queue').hide();
-
-};
 
 
 var initStubs = function(){
@@ -54,11 +15,123 @@ var initStubs = function(){
     
 };
 
+var showAllWorkers = function(e){
+    if (typeof e != 'undefined'){
+        e.preventDefault();
+    }
+    var item = $(this);
+    if(! $(item).hasClass('queue_item')){
+        item = $(item).parents('.queue_item');
+    }
+    $(item).find('.worker_details').each(function(){
+        $(this).find('worker_stats').show().end().find('graph_holder').show().end().find('.small_stats').hide();
+    });
+};
+
+var worker = {
+    item: null,
+    is_null: function(){
+        return typeof (this.item) != 'object';
+    },
+    collapse: function(){
+        if (this.is_null){
+            return false;
+        }
+        this.item.find('worker_stats').hide().end().find('graph_holder').hide().end().find('.small_stats').show();
+        return this;
+    },
+    expand: function(){
+        if (this.is_null){
+            return false;
+        }
+        this.item.find('worker_stats').show().end().find('graph_holder').show().end().find('.small_stats').hide();
+        return this;
+    },
+    setItem: function(el){
+        if($(el).hasClass('.worker_details')){
+            this.item = el;
+        }else{
+            this.item = $(el).parents('.worker_details');
+        }
+        return this;
+    }
+};
+
+var queue = {
+    item : null,
+    is_null: function(){
+        return typeof (this.item) != 'object';
+    },
+
+    setQueue: function(el){
+        if($(el).hasClass('queue_item')){
+            this.item = el;
+        }else{
+            this.item = $(el).parents('.queue_item');
+        }
+        return this;
+    },
+
+    collapseWorkers:function(){
+        $(this.item).find('.worker_details').each(function(){
+            worker.setItem(this).collapse();
+        });
+        return this;
+    },
+
+    expandWorkers:function(){
+        $(this.item).find('.worker_details').each(function(){
+            worker.setItem(this).expand();
+        });
+        return this;
+    },
+
+    expand: function(){
+        $(this.item).removeClass('collapsed').find('.collapse_queue').show().end().find('.expand_queue').hide();
+    },
+
+    collapse: function(){
+        $(this.item).addClass('collapsed').find('.collapse_queue').hide().end().find('.expand_queue').show();
+    },
+
+    isExpanded: function(){
+        return $(this.item).hasClass('collapsed');
+    },
+
+
+
+    init: function(){
+        $('#queues_list .queue_item').each(function(){
+            var q = queue.setQueue(this);
+            if(q.isExpanded()){
+                q.expand();
+            }else{
+                q.collapse();
+            }
+        });
+
+        $('#queues_list .expand_queue').live('click', function(e){
+            e.preventDefault();
+            queue.setQueue(this).expand();
+        });
+        
+        $('#queues_list .collapse_queue').live('click', function(e){
+            e.preventDefault();
+            queue.setQueue(this).collapse();
+        });
+    }
+    
+};
+
+
+
+
 
 $(document).ready(function(){
 
     initStubs();
 
-    setQueueStates();
+    queue.init();
 
 });
+
