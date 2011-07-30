28c62a280619a85f0c606a3b3e74279466123575
Alexey S. Kachayev <kachayev@gmail.com>
2011-07-30 18:18:07 +0300

    Full sonar daemon implementation

--- gearoscope/monitor/sonard.py
+++ gearoscope/monitor/sonard.py
@@ -13,7 +13,46 @@ Options:
 """
 
 from sonar import loop
-from sonar.sonard import sonar_factory
+
+from sonar.options import options
+from sonar.agent import AgentPool
+from sonar.remote import Server, pool as ServerPool
+
+from sonar.agents.process import ProcessStatAgent
+from sonar.agents.supervisor import SupervisorAgent, Supervisor
+
+def sonar_factory():
+    def make(options):
+        import ConfigParser
+
+        # Iterate throw all configuration file in order to create
+        # pool of servers and configurate all supervisor objects
+        for section in options.config.sections():
+            if section.find(':') != -1:
+                block, name = section.split(':')
+
+                # Add server objects to server pool
+                if block.lower() == 'server':
+                    params = {'name': name, 'password': None, 'user': 'root'}
+                    params.update(dict(options.config.items(section)))
+
+                    if 'is_default' in params.keys():
+                        del(params['is_default'])
+
+                    try:
+                        is_default = options.config.getboolean(section, 'is_default')
+                    except ConfigParser.NoOptionError:
+                        is_default = False
+
+                    ServerPool.add(Server(**params), is_default=is_default)
+
+        s = loop.Sonar(options)
+        s.add_pool('stat', AgentPool(prototype=ProcessStatAgent, count=3, timeout=0))
+        s.add_agent(SupervisorAgent(Supervisor(server=ServerPool.get('local'), port=9001),
+                                    names=['gearman', 'multiple', 'reverse', 'sum']))
+
+        return s
+    return make
 
 if __name__ == '__main__':
     loop.main(factory=sonar_factory())
