bb16036d05d76b5845fefb64efc1b33e034606b4
Alexey S. Kachayev <kachayev@gmail.com>
2011-07-30 13:10:57 +0300

    Describe tasks which will used as stub env for gearman nodes

--- tests/env_stub/client.py
+++ tests/env_stub/client.py
@@ -5,5 +5,17 @@ Works in infinity loop and periodicly generate
 stupid tasks and push it to gearman server
 
 Usage: ./client [options]
+
+List of generated tasks:
+1. `reverse` - push word of random length to workload
+2. `sum` - push to workload string representation of add operator (for example, `34+15+12`)
+3. `multiple` - push to workload string representation of multiple operator (for example, `34*15*2`)
+
+To prevent situation when all queues will have the same load
+and give user oppurtunities to see on monitor different graphics for each queue,
+generator will add each task randomly.
+
+Gearman node for test runner and frequency of tasks generation
+can be set via general settings.py
 """
 
