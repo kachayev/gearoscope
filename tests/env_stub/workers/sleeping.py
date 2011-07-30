75de20fd0c079aa3393399f05dadaef4dd0f8931
Alexey S. Kachayev <kachayev@gmail.com>
2011-07-30 14:06:17 +0300

    Comments in sleeping worker code

--- tests/env_stub/workers/sleeping.py
+++ tests/env_stub/workers/sleeping.py
@@ -4,7 +4,19 @@ from time import sleep
 from gearman import GearmanWorker
 
 class SleepingGearmanWorker(GearmanWorker):
+    '''
+    Extend standard gearman worker facilities, adding sleeping tic after each jobs poll
+
+    You can use `after_poll` method to add some additional facilities to runnig worker,
+    for example logging, notification and something like this
+    '''
     def after_poll(self, activity):
+        '''
+        Called by parent gearman worker after each poll
+        (we assump that worker is running in loop and can handle serveral jobs worload)
+
+        True is return in order "to say" to worker get next job
+        '''
         sleep(settings.STUB_WORKERS_FREQUENCY)
         return True
 
