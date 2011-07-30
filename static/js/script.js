8cc7a020d38c01e3df6ee804b453a53b7fe9889e
Iurii Ogiienko <iurii.ogiienko@gmail.com>
2011-07-30 15:02:34 +0300

    added default style for collapsing

--- static/js/script.js
+++ static/js/script.js
@@ -5,5 +5,10 @@ $('.add_worker').live('click', function(e) {
     alert("Add worker not ready yet.");
 });
 
-    
+$('.stop_worker, .restart_worker, .delete_worker').live('click', function(e) {
+    e.preventDefault();
+    alert("Add worker not ready yet.");
+});
+
+
 });
