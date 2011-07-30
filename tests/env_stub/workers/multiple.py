ddb38371d24a105a0764c53189853390c577bf5c
Alexey S. Kachayev <kachayev@gmail.com>
2011-07-30 16:28:15 +0300

    Env stub should work in any env configuration

--- tests/env_stub/workers/multiple.py
+++ tests/env_stub/workers/multiple.py
@@ -1,5 +1,14 @@
+import os, sys
+
+# Working directory (current file one)
+WORKDIR = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+
+# This will give us oppurtunities to keep applications in separated directory
+# and prevent chaus in main project directory
+sys.path.append(os.path.join(WORKDIR, '..'))
+
 import operator
-import tests.settings as settings
+import gearoscope.tests.settings as settings
 
 from sleeping import run
 
--- tests/env_stub/workers/sleeping.py
+++ tests/env_stub/workers/sleeping.py
@@ -1,4 +1,13 @@
-import tests.settings as settings
+import os, sys
+
+# Working directory (current file one)
+WORKDIR = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+
+# This will give us oppurtunities to keep applications in separated directory
+# and prevent chaus in main project directory
+sys.path.append(os.path.join(WORKDIR, '..'))
+
+import gearoscope.tests.settings as settings
 
 from time import sleep
 from gearman import GearmanWorker
