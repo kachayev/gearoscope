e6091633e2d85d0cde992f808a3850dad0a1c561
Alexey S. Kachayev <kachayev@gmail.com>
2011-07-31 15:26:36 +0300

    Fix code formatting for fields in worker model

--- gearoscope/apps/scoper/models.py
+++ gearoscope/apps/scoper/models.py
@@ -144,40 +144,40 @@ class Worker(models.Model):
     name = models.CharField(max_length=70)
 
     # General required params
-    command = models.CharField(max_length=255)
+    command      = models.CharField(max_length=255)
     process_name = models.CharField(max_length=70)
-    numprocs = models.PositiveSmallIntegerField(default=1)
+    numprocs     = models.PositiveSmallIntegerField(default=1)
 
     # Field which describe process running params and env variables
-    directory = models.CharField(max_length=255, default='/', blank=True, null=True)
-    umask = models.CharField(max_length=4, default='022', blank=True, null=True)
-    user = models.CharField(max_length=255, blank=True, null=True)
-    priority = models.PositiveIntegerField(default=999, blank=True, null=True)
+    directory   = models.CharField(max_length=255, default='/', blank=True, null=True)
+    umask       = models.CharField(max_length=4, default='022', blank=True, null=True)
+    user        = models.CharField(max_length=255, blank=True, null=True)
+    priority    = models.PositiveIntegerField(default=999, blank=True, null=True)
     environment = models.CharField(max_length=255, default='', blank=True)
 
     # Param for starting and restarting process
-    autostart = models.BooleanField(default=True)
-    autorestart = models.BooleanField(default=True)
-    startsecs = models.PositiveSmallIntegerField(default=10)
+    autostart    = models.BooleanField(default=True)
+    autorestart  = models.BooleanField(default=True)
+    startsecs    = models.PositiveSmallIntegerField(default=10)
     startretries = models.PositiveSmallIntegerField(default=3)
     stopwaitsecs = models.PositiveIntegerField(default=10)
 
     # Exit params (should tell supervisor how to stop and restart process)
     # and what stop suggest as normal one (non-error)
-    exitcodes = models.CommaSeparatedIntegerField(default='0,2', max_length=24)
+    exitcodes  = models.CommaSeparatedIntegerField(default='0,2', max_length=24)
     stopsignal = models.IntegerField(default=SIGTERM, choices=SIGNAL_CHOICES)
 
     # Params for logging of proces STDOUT and STDERR
     # Useful in order if we want to get log tails via XML-RPC protocol
-    redirect_stderr = models.BooleanField(default=False)
-    stdout_logfile = models.CharField(max_length=255, blank=True, null=True)
-    stdout_logfile_maxbytes = models.PositiveIntegerField(default=1, blank=True, null=True)
-    stdout_logfile_backups = models.PositiveIntegerField(default=10, blank=True, null=True)
-    stdout_capture_maxbytes = models.PositiveIntegerField(default=1, blank=True, null=True)
-    stderr_logfile = models.CharField(max_length=50, blank=True)
-    stderr_logfile_maxbytes = models.PositiveIntegerField(default=1, blank=True, null=True)
-    stderr_logfile_backups = models.PositiveIntegerField(default=10, blank=True, null=True)
-    stderr_capture_maxbytes = models.PositiveIntegerField(default=1, blank=True, null=True)
+    redirect_stderr         = models.BooleanField(default=False)
+    stdout_logfile          = models.CharField(max_length=255, blank=True, null=True)
+    stdout_logfile_maxbytes = models.PositiveIntegerField(default=1,  blank=True, null=True)
+    stdout_logfile_backups  = models.PositiveIntegerField(default=10, blank=True, null=True)
+    stdout_capture_maxbytes = models.PositiveIntegerField(default=1,  blank=True, null=True)
+    stderr_logfile          = models.CharField(max_length=50, blank=True)
+    stderr_logfile_maxbytes = models.PositiveIntegerField(default=1,  blank=True, null=True)
+    stderr_logfile_backups  = models.PositiveIntegerField(default=10, blank=True, null=True)
+    stderr_capture_maxbytes = models.PositiveIntegerField(default=1,  blank=True, null=True)
 
     def __unicode__(self):
         '''
