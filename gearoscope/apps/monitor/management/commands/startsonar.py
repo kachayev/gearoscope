2e42cea9a81a40ece19427b64a0bc92a96ba7fab
Alexey S. Kachayev <kachayev@gmail.com>
2011-07-31 18:59:55 +0300

    Full implementation for sonar starter command for django admin

--- gearoscope/apps/monitor/management/commands/startsonar.py
+++ gearoscope/apps/monitor/management/commands/startsonar.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python
+import sys
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
+from sonar import loop
+from sonar.logger import Log
+from sonar.options import options
+
 from monitor.sonard import sonar_factory
 
+options.define('--settings', '-s')
+sys.argv.append('--config=%s' % settings.SONAR_CONFIGURATION_FILE)
+
 class Command(BaseCommand):
+    help = 'Start sonar-based monitoring daemon'
+
     def handle(self, *args, **options):
-        Log.BUFFER_FILE = settings.SONAR_CONFIGURATION_FILE
+
+        Log.BUFFER_FILE = settings.SONAR_LOG_FILE
         loop.main(factory=sonar_factory())
-        self.stdout.write('Successfully closed poll \n')
 
