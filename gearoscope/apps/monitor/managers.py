d296c12a156101580f5cd5702d3ebd72dfd8522e
Alexey S. Kachayev <kachayev@gmail.com>
2011-08-07 20:11:13 +0300

    Comment for last revision manager object

--- gearoscope/apps/monitor/managers.py
+++ gearoscope/apps/monitor/managers.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 class LastRevisionManager(models.Manager):
+    '''
+    Return QuerySet object with changed order
+    and command for returning only values, not objects.
+    Operating with revision values will save application
+    from bugs during trying to resave existen object
+    '''
     def get_query_set(self):
         return super(LastRevisionManager, self).get_query_set().order_by('-time').values()
 
